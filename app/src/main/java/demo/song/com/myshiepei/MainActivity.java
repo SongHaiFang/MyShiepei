d4a3a9ca17b54c8378ff2d39ec7f87299ad92d74
Cor6 <1912167917@qq.com>
2017-09-26 09:28:18 +0800

    Update MainActivity.java

--- app/src/main/java/demo/song/com/myshiepei/MainActivity.java
+++ app/src/main/java/demo/song/com/myshiepei/MainActivity.java
@@ -13,13 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
-    public void but(View v){
-        Intent addShortCut = new Intent("com.android.launcher.action.INSTALL_SHORTCUT");
-        addShortCut.putExtra(Intent.EXTRA_SHORTCUT_NAME, "aaaas");
-        // 不允许重复创建
-        addShortCut.putExtra("duplicate", false);
-        addShortCut.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,R.mipmap.ic_launcher);
-        addShortCut.putExtra(Intent.EXTRA_SHORTCUT_INTENT, addShortCut);
-        sendBroadcast(addShortCut);
-    }
+   
+    
 }
